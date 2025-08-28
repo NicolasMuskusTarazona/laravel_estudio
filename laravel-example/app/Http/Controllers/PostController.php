98e5c40c1623ceb6e6f69eb3f8fea9243080b029
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-08-28 14:02:33 +0000

    revert:

--- laravel-example/app/Http/Controllers/PostController.php
+++ laravel-example/app/Http/Controllers/PostController.php
@@ -1,16 +1,57 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Traits\ApiResponse;
 
 class PostController extends Controller
 {
     use ApiResponse;
-
-    public function __invoke()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return $this->ok("Desde invoke");
+        // La mala practica porque tenemos un Model
+        return $this->ok("Todo ok", Post::get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $newPost = Post::created($request->only(['title','content','status']));
+        return $this->ok("Todo bien",$newPost);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $result = Post::find($id);
+        if($result){
+            return $this->ok("todo ok",$result);
+        }else{
+            return $this->success("Todo mal",[],404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- laravel-example/app/Models/Post.php
+++ laravel-example/app/Models/Post.php
@@ -7,6 +7,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDelete;
     protected $table = "posts";
+
+    protected $fillable = [
+        'title','content','status'
+        'published_at', 'conver_image', 'tags', 'metadss'
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'tags' => 'array',
+        'meta' => 'meta'
+    ];
+
+    public function categories(){
+        return $this->belongsToMany(Category::class)->using(CategoryPost::class)->withTimestamps();
+    }
 }
